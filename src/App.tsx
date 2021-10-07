28edcc4b69a62a77d21aa1d4b3cd485230ce692e
YM <a834319313@163.con>
2021-10-07 16:40:12 +0800

    ä½1-8æ：使尝试个简单的导航页

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
 import React from 'react';
-// eslint-disable-next-line
-import logo from './logo.svg';
-import './App.css';
-import styled from 'styled-components';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link, Redirect
+} from 'react-router-dom';
 function App() {
   return (
-    <div></div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          {/*先对前面的路径进行匹配，除了上面这些路径之外，剩下的所有其它路径，都显示为找不到内容。*/}
+          <Route path="*">
+            <NoMatch />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+function NoMatch() {
+  return <h2>该路径找不到，页面不存在</h2>;
+}
+export default App
