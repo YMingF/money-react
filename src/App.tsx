fabfaeac882e56f55962786e488aee66be2eb91b
YM <a834319313@163.con>
2021-10-07 17:09:53 +0800

    1-10:完成导航栏的布局

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,32 @@ import {
   Route,
   Link, Redirect
 } from 'react-router-dom';
+import styled from 'styled-components';
+const Wrapper=styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`
+const Main=styled.div`
+    flex-grow: 1;
+    overflow: auto;
+`
+const Nav=styled.nav`
+  >ul{
+    display: flex;
+    >li{
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签</Link>
-            </li>
-            <li>
-              <Link to="/money">记账</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
+      <Wrapper>
+        <Main>
+          <Switch>
           <Route path="/tags">
             <Tags />
           </Route>
@@ -39,7 +46,21 @@ function App() {
             <NoMatch />
           </Route>
         </Switch>
-      </div>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
     </Router>
   );
 }
