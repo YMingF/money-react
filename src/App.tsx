cc6ed3f7254792a9c11dbcf3d47d9f54cfd79479
YM <a834319313@163.con>
2021-10-07 16:04:14 +0800

    删掉一æ无用文件

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 // eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
-import Button from 'components/Button'
 import styled from 'styled-components';
-import Box from './components/Box';
 
 function App() {
   return (
-    <Box>
-      <Button>你好呀，我是一个项目哈哈哈哈哈</Button>
-    </Box>
+    <div></div>
   );
 }
 
--- src/components/Box.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const Box=styled.div`
-      border:3px solid red;
-      box-shadow: 3px 3px 3px 0 rgba(0,0,0,0.5);
-`
-export  default  Box;
