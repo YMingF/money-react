c4e9069a3195c0ae873aa3d70474a44d6b79de5d
YM <a834319313@163.con>
2021-10-08 18:58:41 +0800

    2-2：导入并展示了3个icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {
   HashRouter as Router,
   Switch,
   Route,
+  // eslint-disable-next-line
   Link, Redirect
 } from 'react-router-dom';
 import styled from 'styled-components';
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-
+import x from 'icons/money.svg'
+require('icons/money.svg')
+require('icons/tag.svg')
+require('icons/chart.svg')
+console.log(x);
 const NavWrapper=styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -10,7 +14,14 @@ const NavWrapper=styled.nav`
     >li{
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 4px 0;
+      .icon{
+          width: 24px;
+          height: 24px;
+      }
     }
   }
 `
@@ -19,12 +30,21 @@ const Nav=()=>{
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref='#tag' />
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref='#money' />
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref='#chart' />
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
