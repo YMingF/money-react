b5a8b15863ff7f4aceb59a1e57223669f3a6f65f
YM <a834319313@163.con>
2021-10-08 17:23:03 +0800

    测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link, Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+import  Nav from 'components/Nav'
+const Wrapper=styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+   
+`
+const Main=styled.div`
+    flex-grow: 1;
+    overflow: auto;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money/>
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+          {/*先对前面的路径进行匹配，除了上面这些路径之外，剩下的所有其它路径，都显示为找不到内容。*/}
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+function NoMatch() {
+  return <h2>该路径找不到，页面不存在</h2>;
+}
+export default App
