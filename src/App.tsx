80f491ec544955d7ce5668e7e942137f77b533e9
YM <a834319313@163.con>
2021-10-09 16:05:01 +0800

    3-3:Money.tsx的TagSection样式完成

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,33 @@ import Money from './views/Money';
 import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
 
+const AppWrapper=styled.div`
+  color:#333
+`
 function App() {
   return (
-    <Router>
-          <Switch>
-          <Route path="/tags">
-            <Tags />
-          </Route>
-          <Route path="/money">
-            <Money/>
-          </Route>
-          <Route path="/statistics">
-            <Statistics />
-          </Route>
-          <Redirect exact from="/" to="/money" />
-          {/*先对前面的路径进行匹配，除了上面这些路径之外，剩下的所有其它路径，都显示为找不到内容。*/}
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+            <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            {/*先对前面的路径进行匹配，除了上面这些路径之外，剩下的所有其它路径，都显示为找不到内容。*/}
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 export default App
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,15 +4,27 @@ import styled from 'styled-components';
 
 //css in js的使用
 const TagsSection=styled.section`
-
+  background: #FFFFFF;
+  padding: 12px 16px;
+  >ol{
+    margin: 0 -12px;
+    >li{
+      background: #d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin:8px 12px; //让标签的左右两边各12px所以两个标签间就间隔24px
+    }
+  }
+  >button{
+    background: none; border: none; padding: 2px 4px;
+    border-bottom: 1px solid #333; color:#666;
+    margin-top: 8px;
+  }
 `
-const NotesSection=styled.section`
-
-`
-
+const NotesSection=styled.section``
 const CategorySection=styled.section``
-
-
 const NumberPadSection=styled.section``
 
 function Money() {
