b392f20e548b7e068682895e4dcdb23e27f9d242
YM <a834319313@163.con>
2021-10-09 17:32:48 +0800

    3-5：Money页面收入支出样式的设计

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -40,7 +40,29 @@ const NotesSection=styled.section`
     }
   }
 `
-const CategorySection=styled.section``
+const CategorySection=styled.section`
+  font-size: 24px;
+  >ul{
+    display: flex;
+    background-color: #c4c4c4;
+    >li{
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      &.selected::after{
+        content:"";
+        display: block;
+        height: 3px;
+        background-color: #333;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width:100%;//加了绝对定位的元素宽度会成为0,所以要加这个
+      }
+    }
+  }
+`
 const NumberPadSection=styled.section``
 
 function Money() {
@@ -63,7 +85,7 @@ function Money() {
       </NotesSection>
       <CategorySection >
         <ul>
-          <li>支出</li>
+          <li className='selected'>支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
