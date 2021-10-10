3e1d24eee671d72ff61623282ca0505c04a33045
YM <a834319313@163.con>
2021-10-10 14:28:51 +0800

    4-5：实现支出收入的切换功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -17,16 +17,9 @@ const MyLayout=styled(Layout)`
 function Money() {
   return (
     <MyLayout>
-      <TagsSection >
-
-        </TagsSection>
+      <TagsSection />
       <NoteSection />
-      <CategorySection >
-        <ul>
-          <li className='selected'>支出</li>
-          <li>收入</li>
-        </ul>
-      </CategorySection>
+      <CategorySection/>
       <NumberPadSection >
         <div className='output'>100</div>
         <div className='pad clearFix'>
