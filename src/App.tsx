3c4c03ed8afd331e1894e245cf77841f81c7b862
YM <a834319313@163.con>
2021-10-12 16:04:19 +0800

    6-3：创建/tags/:tag路由

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Statistics from './views/Statistics';
 import Tags from './views/Tags';
 import NoMatch from './views/NoMatch';
 import styled from 'styled-components';
+import {Tag} from './views/Tag';
 
 const AppWrapper=styled.div`
   color:#333
@@ -19,19 +20,22 @@ function App() {
   return (
     <AppWrapper>
       <Router>
-            <Switch>
-            <Route path="/tags">
+          <Switch>
+            <Route  exact path="/tags" >
               <Tags />
             </Route>
-            <Route path="/money">
+            <Route  exact path="/tags/:tag" >
+              <Tag />
+            </Route>
+            <Route  exact path="/money" >
               <Money/>
             </Route>
-            <Route path="/statistics">
+            <Route  exact path="/statistics" >
               <Statistics />
             </Route>
             <Redirect exact from="/" to="/money" />
             {/*先对前面的路径进行匹配，除了上面这些路径之外，剩下的所有其它路径，都显示为找不到内容。*/}
-            <Route path="*">
+            <Route path="*" >
               <NoMatch />
             </Route>
           </Switch>
--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,16 +3,19 @@ import React from 'react';
 import {useTags} from '../useTags';
 import styled from 'styled-components';
 import Icon from '../components/icon';
+import { Link } from 'react-router-dom';
 const TagList=styled.ol`
   font-size: 16px;
   background-color: white;
   >li{
     border-bottom: 1px solid #d5d5d9;
     line-height: 20px;
-    padding: 12px 16px ;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;  
+    >a{
+      padding: 12px 16px ;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
   }
 `
 const Button=styled.button`
@@ -40,8 +43,10 @@ function Tags() {
       <TagList>
         {tags.map(tag=>
             <li key={tag}>
-              <span className='oneLine'>{tag}</span>
-              <Icon name='right'/>
+              <Link to={'/tags/'+tag}>
+                  <span className='oneLine'>{tag}</span>
+                  <Icon name='right'/>
+              </Link>
             </li>
           )
         }
