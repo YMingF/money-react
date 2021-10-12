9fdf7bc9ef3815b81a5a6cf531d9ea4af9013d14
YM <a834319313@163.con>
2021-10-12 16:58:15 +0800

    6-6:解决每次刷新标签页id就增加5的问题

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,15 +1,13 @@
 import {useState} from 'react';
 import {createId} from './lib/createId';
-
+const defaultTags=[
+  {id:createId(),name:'衣'},
+  {id:createId(),name:'食'},
+  {id:createId(),name:'住'},
+  {id:createId(),name:'行'}
+]
 const useTags=()=>{//封装自定义Hook
-  const [tags,setTags]=useState<{id:number;name:string}[]>(
-    [
-      {id:createId(),name:'衣'},
-      {id:createId(),name:'食'},
-      {id:createId(),name:'住'},
-      {id:createId(),name:'行'}
-    ]
-  )
+  const [tags,setTags]=useState<{id:number;name:string}[]>(defaultTags)
   return {  tags:tags,setTags:setTags  } //只能返回对象，而非数组，否则会报错.
 }
 
