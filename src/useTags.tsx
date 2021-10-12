176487e1040e296704d22fb0646d63833e0c49ac
YM <a834319313@163.con>
2021-10-12 15:48:48 +0800

    6-2:tagList样式设置完

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,10 +1,8 @@
 import {useState} from 'react';
-const useTags=()=>{
+
+const useTags=()=>{//封装自定义Hook
   const [tags,setTags]=useState<string[]>(['衣','食', '住','行'])
-  return {
-    tags:tags,
-    setTags:setTags
-  }
+  return {  tags:tags,setTags:setTags  } //只能返回对象，而非数组，否则会报错.
 }
 
 export  {useTags}
